d7056c10ad9d3e1c527093a0ac86239ea9a5db61
Drew Cavanaugh <rcavana1@uncc.edu>
2020-04-14 19:07:25 -0400

    SocketHelper testing

--- Program.cs
+++ Program.cs
@@ -5,131 +5,92 @@ using System.Text;
 
 namespace Sockets {
     class Program {
-        public static string data = null;
+        private static SocketHelper socket;
 
         static void Main(string[] args) {
-            if (args.Length > 0 && args[0] == "server") {
-                Console.WriteLine("Starting server...");
-                Console.WriteLine("Press Ctrl+C to quit.\n");
-                Listen();
-            } else {
-                Client();
-            }
-        }
-
-        private static void Client() {
-            // 1kb socket buffer
-            var bytes = new byte[1024];
-            // connect to remote server
-            var ipHost = Dns.GetHostEntry(Dns.GetHostName());
-
-            Console.WriteLine("Loopback address: {0}", ipHost.AddressList[0].ToString());
-            IPAddress ipAddr;
-            bool valid;
-
-            do {
-                Console.Write("Enter IP Address to connect to: ");
-                var input = Console.ReadLine();
-
-                valid = IPAddress.TryParse(input, out ipAddr);
-            } while (!valid);
-            var endPoint = new IPEndPoint(ipAddr, 11000);
-            // create sender socket
-            var sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
             try {
-                sender.Connect(endPoint);
-                // get client's name and send it to the server
-                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
-                Console.Write("Enter your name: ");
-                var name = Console.ReadLine();
-                // append <EOF> flag to mark the end of the message
-                var msg = Encoding.ASCII.GetBytes(string.Concat(name, "<EOF>"));
-                var sent = sender.Send(msg);
-
-                Console.WriteLine("\tMessage sent: My name is {0}", name);
-                // clear data string and wait for response
-                data = null;
-                while (true) {
-                    var rec = sender.Receive(bytes);
-
-                    data += Encoding.ASCII.GetString(bytes, 0, rec);
-                    // <EOF> flag marks the end of the message, so break here and remove it from the final output
-                    if (data.IndexOf("<EOF>") > -1) {
-                        data = data.Remove(data.IndexOf("<EOF>"));
-                        break;
+                if (args.Length > 0 && args[0] == "server") {
+                    Console.WriteLine("Starting server...");
+                    Console.WriteLine("Press Ctrl+C to quit.\n");
+                    socket = new SocketHelper(true);
+                    BindIPPrompt();
+                    Console.WriteLine("Waiting for connection...");
+                    socket.WaitForConnection();
+                    Console.WriteLine("Connection established! Press Ctrl+C to quit.");
+                    while (socket.IsConnected) {
+                        Console.WriteLine("Waiting for message...");
+                        socket.Receive();
+                        Console.WriteLine("\t{0} says: {1}", socket.RemoteEndpoint, socket.Message);
+                        Console.Write("Type a return message: ");
+                        socket.Message = Console.ReadLine();
+                        Console.WriteLine("\t{0} (you) says: {1}", socket.LocalEndpoint, socket.Message);
+                        socket.Send();
+                    }
+                } else {
+                    socket = new SocketHelper(false);
+                    ConnectToIPPrompt();
+                    Console.WriteLine("Connection established! Press Ctrl+C to quit.");
+                    while (socket.IsConnected) {
+                        Console.Write("Type a return message: ");
+                        socket.Message = Console.ReadLine();
+                        Console.WriteLine("\t{0} (you) says: {1}", socket.LocalEndpoint, socket.Message);
+                        socket.Send();
+                        Console.WriteLine("Waiting for message...");
+                        socket.Receive();
+                        Console.WriteLine("\t{0} says: {1}", socket.RemoteEndpoint, socket.Message);
                     }
                 }
-                Console.WriteLine("\tMessage received: {0}", data);
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
             } catch (Exception e) {
-                Console.WriteLine("Error! {0}", e.Message);
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press enter to exit...");
+                Console.ReadLine();
             }
-            Console.WriteLine("Press any key to exit...");
-            Console.Read();
         }
 
-        private static void Listen() {
-            // 1kb socket buffer
-            var bytes = new byte[1024];
-            // local server endpoint for this socket
-            var ipHost = Dns.GetHostEntry(Dns.GetHostName());
+        private static void BindIPPrompt() {
+            var ips = socket.GetIPAddresses();
 
+            Console.Clear();
             Console.WriteLine("IP Addresses available:");
-            for (var i = 0; i < ipHost.AddressList.Length; i++) {
-                Console.WriteLine("[{0}] {1}", i, ipHost.AddressList[i]);
+            for (var i = 0; i < ips.Count; i++) {
+                Console.WriteLine("\t[{0}]: {1}", i, ips[i]);
             }
             int index;
             bool valid;
+
             do {
-                Console.Write("Which index to bind to? ");
+                Console.Write("Bind to which IP? Enter index: ");
                 var input = Console.ReadLine();
 
                 valid = int.TryParse(input, out index);
-                if (index > ipHost.AddressList.Length) {
+                if (index >= ips.Count) {
                     valid = false;
                 }
             } while (!valid);
-            var ipAddr = ipHost.AddressList[index];
-            var endPoint = new IPEndPoint(ipAddr, 11000);
-            // socket listener
-            var listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try {
+                socket.BindToAddress(ips[index]);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press enter to try again or Ctrl+C to quit...");
+                Console.ReadLine();
+                // recursively try again
+                BindIPPrompt();
+            }
+        }
+
+        private static void ConnectToIPPrompt() {
+            Console.Clear();
+            Console.Write("Enter IP Address to connect to: ");
+            var input = Console.ReadLine();
 
             try {
-                // bind to port and wait for client to send a message
-                listener.Bind(endPoint);
-                listener.Listen(10);
-                Console.WriteLine("Listener bound to {0}", listener.LocalEndPoint.ToString());
-                while (true) {
-                    Console.WriteLine("Waiting for connection...");
-                    var handler = listener.Accept();
-
-                    Console.WriteLine("Connection established, awaiting message...");
-                    data = null;
-                    while (true) {
-                        var rec = handler.Receive(bytes);
-
-                        data += Encoding.ASCII.GetString(bytes, 0, rec);
-                        // <EOF> flag marks the end of the message, so break here and remove it from the final output
-                        if (data.IndexOf("<EOF>") > -1) {
-                            data = data.Remove(data.IndexOf("<EOF>"));
-                            break;
-                        }
-                    }
-                    Console.WriteLine("\tMessage received: {0}", data);
-                    // reply to the client with Hello, [name]
-                    var reply = string.Format("Hello, {0}", data);
-                    // append <EOF> flag to mark the end of the message
-                    var msg = Encoding.ASCII.GetBytes(string.Concat(reply, "<EOF>"));
-
-                    Console.WriteLine("\tMessage sent: {0}", reply);
-                    handler.Send(msg);
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
-                }
-            } catch (Exception e) {
-                Console.WriteLine("Error! {0}", e.Message);
+                socket.EstablishConnection(input);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press enter to try again or Ctrl+C to quit...");
+                Console.ReadLine();
+                // recursively try again
+                ConnectToIPPrompt();
             }
         }
     }
---
+++ SocketHelper.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sockets {
+    /// <summary>
+    /// The Socket Helper class for sending and receiving packets in the Tic Tac Toe Program
+    /// <para>Author: Drew Cavanaugh, 2020</para>
+    /// </summary>
+    public class SocketHelper {
+        public const ushort PORT = 11000;
+        public const byte BUFFER = 32;
+        public const string END_FLAG = "<END>";
+
+        public bool IsHost { get; private set; }
+        public bool IsConnected {
+            get {
+                return listener == null ? false : listener.Connected;
+            }
+        }
+        public string RemoteEndpoint {
+            get {
+                return listener == null ? string.Empty : listener.RemoteEndPoint.ToString();
+            }
+        }
+        public string LocalEndpoint {
+            get {
+                return sender == null ? string.Empty : sender.LocalEndPoint.ToString();
+            }
+        }
+        public string Message { get; set; }
+
+        private EndPoint endPoint;
+        private Socket sender, listener;
+        private byte[] buffer;
+
+        /// <summary>
+        /// Initializes a new SocketHelper class for Tic-Tac-Toe.
+        /// </summary>
+        /// <param name="isHost">Whether this instance is acting as the host (true) or client (false).</param>
+        public SocketHelper(bool isHost) {
+            IsHost = isHost;
+            Message = string.Empty;
+            buffer = new byte[BUFFER];
+        }
+
+        /// <summary>
+        /// Returns a string List of all IP Addresses this SocketHelper can bind to.
+        /// </summary>
+        public List<string> GetIPAddresses() {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            var addrList = new List<string>();
+
+            foreach (var ip in host.AddressList) {
+                addrList.Add(ip.ToString());
+            }
+            return addrList;
+        }
+
+        /// <summary>
+        /// Host only: binds the application to the specified IP address and port.
+        /// </summary>
+        /// <param name="ip">The IP Address to bind to.</param>
+        /// <exception cref="InvalidOperationException">If the specified IP is not available to be binded to.</exception>
+        /// <exception cref="SocketException">If it is unable to bind to the IP and port.</exception>
+        public void BindToAddress(string ip) {
+            if (!IsHost) {
+                // method is only for hosts
+                throw new InvalidOperationException("This method should only be called by the host!");
+            }
+            if (IsConnected) {
+                // can't bind to new address while we're connected
+                throw new InvalidOperationException(string.Format("The socket is already connected to {0}!", sender.RemoteEndPoint.ToString()));
+            }
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            var valid = IPAddress.TryParse(ip, out var addr);
+
+            if (!valid) {
+                throw new ArgumentException(string.Format("{0} is not a valid IPv4 Address.", ip));
+            }
+            if (!host.AddressList.Any(a => a.Equals(addr))) {
+                throw new InvalidOperationException(string.Format("Cannot bind to {0} because it is not reserved for this machine.", ip));
+            }
+            endPoint = new IPEndPoint(addr, PORT);
+            sender = new Socket(addr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sender.Bind(endPoint);
+            sender.Listen(1);
+        }
+
+        /// <summary>
+        /// HOST ONLY: Waits for a client to connect.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If the socket connection is already established.</exception>
+        public void WaitForConnection() {
+            if (!IsHost) {
+                // method is only for hosts
+                throw new InvalidOperationException("This method should only be called by the host!");
+            }
+            if (IsConnected) {
+                // can't wait for new connection while we're connected
+                throw new InvalidOperationException(string.Format("The socket is already connected to {0}!", sender.RemoteEndPoint.ToString()));
+            }
+            while (!IsConnected) {
+                // waiting for connection
+                listener = sender.Accept();
+            }
+        }
+
+        /// <summary>
+        /// CLIENT ONLY: Connects to a host IP.
+        /// </summary>
+        /// <param name="ip">The IP Address of the Host.</param>
+        /// <exception cref="InvalidOperationException">If the socket connection is already established.</exception>
+        /// <exception cref="ArgumentException">If the IP parameter is not a valid IPv4 Address.</exception>
+        public void EstablishConnection(string ip) {
+            if (IsHost) {
+                // method is only for clients
+                throw new InvalidOperationException("This method should only be called by the client!");
+            }
+            if (IsConnected) {
+                // can't establish new connection while we're connected
+                throw new InvalidOperationException(string.Format("The socket is already connected to {0}", sender.RemoteEndPoint.ToString()));
+            }
+            var valid = IPAddress.TryParse(ip, out var addr);
+            
+            if (!valid) {
+                throw new ArgumentException(string.Format("{0} is not a valid IPv4 Address.", ip));
+            }
+            endPoint = new IPEndPoint(addr, PORT);
+            sender = new Socket(addr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(endPoint);
+        }
+
+        /// <summary>
+        /// Sends the data in Message field to the remote endpoint.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If the socket connection is not established.</exception>
+        /// <exception cref="NullReferenceException">If the Message field is null, empty, or white spaces.</exception>
+        public void Send() {
+            if (!IsConnected) {
+                // cannot send message if there is no connection
+                throw new InvalidOperationException(string.Format("The socket is not connected!"));
+            }
+            if (string.IsNullOrWhiteSpace(Message)) {
+                throw new NullReferenceException(string.Format("Cannot send a null or empty Message!"));
+            }
+            var raw = Encoding.ASCII.GetBytes(string.Concat(Message, END_FLAG));
+
+            sender.Send(raw);
+        }
+
+        /// <summary>
+        /// Waits for the remote endpoint to receive a message and stores it in the Message field.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If the socket connection is not established.</exception>
+        public void Receive() {
+            if (!IsConnected) {
+                // cannot receive message if there is no connection
+                throw new InvalidOperationException(string.Format("The socket is not connected!"));
+            }
+            Message = string.Empty;
+            while (true) {
+                var received = sender.Receive(buffer);
+
+                Message.Concat(Encoding.ASCII.GetString(buffer, 0, received));
+                if (Message.EndsWith(END_FLAG)) {
+                    Message.Remove(Message.IndexOf(END_FLAG));
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Closes all socket connections and resets the class.
+        /// </summary>
+        public void CloseConnection() {
+            if (IsConnected) {
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            if (IsHost) {
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+            Message = string.Empty;
+        }
+    }
+}
